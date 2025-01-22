268f69a61e391125888ad1bb60e20fcf94415854
rubenaguilardev <ragui56@wgu.edu>
2025-01-22 00:32:37 -0800

    Add sound on click

--- js/game.js
+++ js/game.js
@@ -1,22 +1,41 @@
 function main() {
+
     let buttonColors = ["red", "blue", "green", "yellow"];
     let gamePattern = [];
+    let userClickedPattern = [];
 
     let randomNumber = nextSequence();
     let randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor)
+    gamePattern.push(randomChosenColor);
 
-    $(`#${randomChosenColor}`).css('filter', 'brightness(1.8)');
+
+    $(`#${randomChosenColor}`).css('filter', 'brightness(2.5)');
     setTimeout(function() {
         $(`#${randomChosenColor}`).css('filter', 'brightness(1)');
     }, 500);
+
+    $('img').click(function() {
+        let userChosenColor = this.id;
+        console.log(userChosenColor)
+        playSound(userChosenColor);
+        userClickedPattern.push(userChosenColor);
+       
+    })
 }
+
 
 
 
 
 function nextSequence() {
     return Math.floor(Math.random() * 4);
+}
+
+
+function playSound(name) {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    console.log(audio)
+    audio.play();
 }
 
 
